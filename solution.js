c5897be4f274f4102e215e5d716f36fce5ed2d50
jordanvillacorta <jordan.villacorta@gmail.com>
2021-06-11 23:36:09 -0400

    added solution to matrix exercism exercise

--- solution.js
+++ solution.js
@@ -5,44 +5,22 @@
 
 export class Matrix {
   constructor(numbers) {
-    let splitNumbers = numbers.split('\n');
-    this.numbersArray = splitNumbers.map(row => row.split(' ').map(entry => +entry));
-    // console.log(splitNumbers.map(row => row.split(' ').map(entry => +entry)));
+    let splitNumbers = numbers.split('\n')
+    this.numbersArray = splitNumbers
+      .map(row => row.split(' ')
+        .map(entry => +entry)
+      )
   }
 
   get rows() {
-    console.log(this.numbersArray)
-    return this.numbersArray;
+    return this.numbersArray
   }
 
   get columns() {
-    return this.numbersArray
+    let columnArray = this.numbersArray[0]
+      .map((entry, index) => this.numbersArray
+        .map(row => row[index])
+      )
+    return columnArray
   }
-  
 }
-
-// const iterable = [10, 20, 30];
-
-// for (let value of iterable) {
-//   value += 1;
-//   console.log(value);
-// }
-// 11
-// 21
-// 31
-
-// const iterable = new Map([['a', 1], ['b', 2], ['c', 3]]);
-
-// for (const entry of iterable) {
-//   console.log(entry);
-// }
-// // ['a', 1]
-// // ['b', 2]
-// // ['c', 3]
-
-// for (const [key, value] of iterable) {
-//   console.log(value);
-// }
-// 1
-// 2
-// 3
